a4b27aa6ed6ff9b39731dc68d0688eb466076b07
ali-ranah <ranasaimali1234@gmail.com>
2024-06-24 13:02:35 +0500

    Updates

--- src/main/java/FlightTicket.java
+++ src/main/java/FlightTicket.java
@@ -2,7 +2,6 @@ public class FlightTicket implements IFlightTicket{
     private String ticketDetails;
 
     public FlightTicket() {
-        // Simulate a heavy resource-consuming operation
         System.out.println("Loading flight ticket details from the system...");
         this.ticketDetails = "Flight Ticket Details: [Flight Number: 123, Departure: XYZ, Arrival: ABC]";
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         IFlightTicket ticketProxy = new FlightTicketProxy();
 
-        // First call, creates the real object
         System.out.println(ticketProxy.getTicketDetails());
 
-        // Subsequent calls use the already created object
         System.out.println(ticketProxy.getTicketDetails());
     }
 }
